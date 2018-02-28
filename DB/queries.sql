541bbf322ac61e47fd9f48ce285e9955deef2dab
Jason Thomo <amjbvic@gmail.com>
2018-02-28 13:58:12 -0800

    Update queries.sql

--- DB/queries.sql
+++ DB/queries.sql
@@ -3,14 +3,21 @@ SELECT *
 FROM restaurants
 ORDER BY reviews DESC;
 
-/* Finding the best restraunts for each category */
-SELECT *
-FROM restaurants JOIN categories USING (id)
-WHERE cat LIKE '%Burgers%' AND reviews >= 20
-ORDER BY rating DESC, reviews DESC;
-
 /* Finding the best types of restraunts */
 SELECT cat, count(id)
 FROM categories
 GROUP BY cat
 ORDER BY count(id) DESC;
+
+/* Finding the best restaurants from all categories */
+SELECT *
+FROM restaurants JOIN categories USING (id)
+WHERE reviews >= 20
+GROUP BY cat
+ORDER BY rating DESC, reviews DESC;
+ 
+/* Finding the best restraunts for each category */
+SELECT *
+FROM restaurants JOIN categories USING (id)
+WHERE cat LIKE '%Cafes%' AND reviews >= 20
+ORDER BY rating DESC, reviews DESC;
