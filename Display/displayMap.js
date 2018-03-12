a57e27f60e318227f7ce8c3710900c97f6704afd
Jason Thomo <amjbvic@gmail.com>
2018-03-11 21:10:40 -0700

    Add files via upload

--- Display/displayMap.js
+++ Display/displayMap.js
@@ -1,15 +1,41 @@
+var map;
+var markersArray = [];
+
 function initMap() {
     // Create a map object and specify the Div element to display it on
     victoria = {lat: 48.428421, lng: -123.365644};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: victoria,
-        zoom: 13
+        zoom: 14,
+        disableDefaultUI: true
     });
+    getXMLData('getData.php?cat=""', map);
 
+    var filtersPanel = document.getElementById('filtersPanel');
+
+    var textField1 = document.getElementById('userInput');
+    function useValue() {
+        clearMarkers();
+
+        var cat = encodeURIComponent('"'+textField1.value+'"');
+        getXMLData('getData.php?cat='+cat, map);
+    }
+    textField1.oninput = useValue;
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(filtersPanel);
+}
+
+function clearMarkers() {
+    for (var i = 0; i < markersArray.length; i++ ) {
+      markersArray[i].setMap(null);
+    }
+    markersArray = [];
+}
+
+function getXMLData(url, map) {
     // Using AJAX to get the XML data from the 'getData.php' file and use it
     // to display markers with info-boxes:
 
-    var url = 'getData.php';
     var request = new XMLHttpRequest; // the main object to request the XML
 
     request.onreadystatechange = function() { // when the request changes state
@@ -22,9 +48,11 @@ function initMap() {
 
                 var id = markerElem.getAttribute('id');
                 var name = markerElem.getAttribute('name');
+                var cat = markerElem.getAttribute('cat');
                 var address = markerElem.getAttribute('address');
                 var reviews = markerElem.getAttribute('reviews');
                 var rating = markerElem.getAttribute('rating');
+
                 var point ={lat: parseFloat(markerElem.getAttribute('lat')),
                             lng: parseFloat(markerElem.getAttribute('lng'))};
     
@@ -33,15 +61,19 @@ function initMap() {
 
                 var title = document.createElement('strong'); // name
                 title.textContent = name;
+                var text0 = document.createElement('text'); // categories
+                text0.textContent = cat;
                 var text1 = document.createElement('text'); // address
                 text1.textContent = address;
-                var text2 = document.createElement('text'); // address
+                var text2 = document.createElement('text'); // reviews
                 text2.textContent = reviews + " reviews";
-                var text3 = document.createElement('text'); // address
+                var text3 = document.createElement('text'); // rating
                 text3.textContent = rating + " stars";
 
                 // appending the text to the info-box
                 markerInfo.appendChild(title);
+                markerInfo.appendChild(document.createElement('br'));
+                markerInfo.appendChild(text0);
                 markerInfo.appendChild(document.createElement('br'));
                 markerInfo.appendChild(text1);
                 markerInfo.appendChild(document.createElement('br'));
@@ -54,6 +86,7 @@ function initMap() {
                     map: map,
                     position: point
                 });
+                markersArray.push(marker);
 
                 // set the info-box to the marker on click
                 infoWindow = new google.maps.InfoWindow;
--- Display/getData.php
+++ Display/getData.php
@@ -7,6 +7,7 @@ $servername="localhost";
 $username="root";
 $password="";
 $conn = mysqli_connect($servername, $username, $password);
+$cat = $_GET['cat'];
 
 function parseToXML($htmlStr) {
     $xmlStr=str_replace('<','&lt;',$htmlStr);
@@ -29,31 +30,54 @@ if (!$db) {
 }
 
 // Select all the rows in the restaurants table
-$query = "SELECT * FROM restaurants";
+$query = "SELECT * 
+          FROM restaurants JOIN categories c1 USING(id) JOIN categories c2 USING(id) 
+          WHERE c1.cat LIKE '%'$cat'%'";
 $result = mysqli_query($conn, $query);
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
 
 
+
 // Start XML file, echo parent node
 echo '<markers>';
 
 // Iterate through the rows, printing XML nodes for each
+$id_prev = -1;
+$cat_str = "";
+
 while ($row = mysqli_fetch_assoc($result)) {
     // Creating each XML node
-    echo '<marker ';
-    echo 'id="' . $row['id'] . '" ';
-    echo 'name="' . parseToXML(utf8_encode($row['name'])) . '" ';
-    echo 'address="' . parseToXML($row['address']) . '" ';
-    echo 'reviews="' . parseToXML($row['reviews']) . '" ';
-    echo 'rating="' . parseToXML($row['rating']) . '" ';
-    echo 'lat="' . $row['latitude'] . '" ';
-    echo 'lng="' . $row['longitude'] . '" ';
-    echo '/>';
+    $id = $row['id'];
+    if ($id != $id_prev) {
+
+        if ($id_prev != -1) {
+            echo 'cat="' . parseToXML(utf8_encode($cat_str)) . '" ';
+            echo '/>';
+        }
+
+        echo '<marker ';
+        echo 'id="' . $row['id'] . '" ';
+        echo 'name="' . parseToXML(utf8_encode($row['name'])) . '" ';
+        echo 'address="' . parseToXML($row['address']) . '" ';
+        echo 'reviews="' . parseToXML($row['reviews']) . '" ';
+        echo 'rating="' . parseToXML($row['rating']) . '" ';
+        echo 'lat="' . $row['latitude'] . '" ';
+        echo 'lng="' . $row['longitude'] . '" ';
+        $new_cat = $row['cat'];
+        $cat_str = "$new_cat";
+        $id_prev = $id;
+    }
+    else {
+        $new_cat = $row['cat'];
+        $cat_str = "$cat_str, $new_cat";
+    }
 }
 
-// End XML file
+// Last XML marker
+echo 'cat="' . parseToXML(utf8_encode($cat_str)) . '" ';
+echo '/>';
 echo '</markers>';
 
 ?>
