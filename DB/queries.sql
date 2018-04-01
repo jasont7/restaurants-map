3f8d9f6d5e6443ee76ec7aa4125225fbdbf462a1
Jason Thomo <amjbvic@gmail.com>
2018-04-01 16:53:49 -0700

    Update queries.sql

--- DB/queries.sql
+++ DB/queries.sql
@@ -21,3 +21,9 @@ SELECT *
 FROM restaurants JOIN categories USING (id)
 WHERE cat LIKE '%Cafes%' AND reviews >= 20
 ORDER BY rating DESC, reviews DESC;
+
+/* Displaying all the categories for a restaurant in a seperate row */
+/* (used in getData.php) */
+SELECT DISTINCT restaurants.*, c2.cat
+FROM restaurants JOIN categories c1 USING(id) JOIN categories c2 USING(id) 
+WHERE c1.cat LIKE '%Pizza%';
