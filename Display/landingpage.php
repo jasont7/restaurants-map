a81716af97f48827cd87c9037c153e48dddd07be
Eric Hasegawa <erichasegawa1@gmail.com>
2018-04-18 13:56:12 -0700

    Update landingpage.php

--- Display/landingpage.php
+++ Display/landingpage.php
@@ -1,9 +1,25 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-    <link rel="stylesheet" href="styles/landingpage.css" />
+<link rel="stylesheet" type="text/css" href="landing-style.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<title>Restaurant Map Landing</title>
 </head>
+
 <body>
-    
+
+<img id = 'logo'  src = "images/logo2.png">
+<div id = "text">
+<h1> I want _________ </h1> 
+</div>
+<!--
+<div id = 'dropdown'>
+<h2 style ="text-align: center" > Select your city! </h2>
+<select id = 'citySelector'>
+<option value = 'victoria'> Victoria BC, Canada </option>
+<option value = 'tofino'> Tofino BC, Canada </option> 
+!-->
+</div>
+
 </body>
 </html>
