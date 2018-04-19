f3342159adfbf5e6d9fc80028f445d012da893f4
Jason Thomo <amjbvic@gmail.com>
2018-04-18 19:05:33 -0700

    Update landingpage.php

--- Display/landingpage.php
+++ Display/landingpage.php
@@ -1,25 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="landing-style.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<title>Restaurant Map Landing</title>
+    <title>FoodAdvisor</title>
+    <link rel="stylesheet" href="styles/landingpage.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet">
 </head>
-
 <body>
+    <img id="logo" src="images/logo.png">
 
-<img id = 'logo'  src = "images/logo2.png">
-<div id = "text">
-<h1> I want _________ </h1> 
-</div>
-<!--
-<div id = 'dropdown'>
-<h2 style ="text-align: center" > Select your city! </h2>
-<select id = 'citySelector'>
-<option value = 'victoria'> Victoria BC, Canada </option>
-<option value = 'tofino'> Tofino BC, Canada </option> 
-!-->
-</div>
-
+    <div id = "text">
+        <h1>I want... ________</h1> 
+    </div>
+    <!--
+    <div id = 'dropdown'>
+        <h2 style ="text-align: center" > Select your city! </h2>
+        <select id = 'citySelector'>
+        <option value = 'victoria'> Victoria BC, Canada </option>
+        <option value = 'tofino'> Tofino BC, Canada </option> 
+    </div>
+    !-->
 </body>
 </html>
