0d6c325ad4de04bb7e111f28626b0a572d757178
Jason Thomo <amjbvic@gmail.com>
2018-04-19 19:16:46 -0700

    Update landingpage.php

--- Display/landingpage.php
+++ Display/landingpage.php
@@ -1,48 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="styles/landing-style.css">
-<!--<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
-<title>Restaurant Map Landing</title>
+    <title>FoodAdvisor</title>
+    <link rel="stylesheet" href="styles/landingpage.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet">
 </head>
-
 <body>
+    <img id="logo" src="images/logo.png">
 
-<img id = 'logo'  src = "images/logo2.png">
-<div id = "text">
-<h1> I want _________ </h1> 
-</div>
-<h1>Pure CSS3 Text Carousel</h1>
-
-<div class="content-slider">
-  <div class="slider">
-    <div class="mask">
-      <ul>
-        <li class="anim1">
-          <div class="quote">Hello, this is a quote from a person.</div>
-          <div class="source">- Person</div>
-        </li>
-        <li class="anim2">
-          <div class="quote">Hello, this is a quote from another person.</div>
-          <div class="source">- Another person</div>
-        </li>
-        <li class="anim3">
-          <div class="quote">Hello, this is a quote from an animal.</div>
-          <div class="source">- Animal</div>
-        </li>
-        <li class="anim4">
-          <div class="quote">Hello, this is a quote from a plant.</div>
-          <div class="source">- Plant</div>
-        </li>
-        <li class="anim5">
-          <div class="quote">How do ya like that.</div>
-          <div class="source">- Cassidy</div>
-        </li>
-      </ul>
+    <div id="text">
+        <h1>I want... ________</h1> 
     </div>
-  </div>
-</div>
-
+    <!--
+    <div id = 'dropdown'>
+        <h2 style ="text-align: center" > Select your city! </h2>
+        <select id = 'citySelector'>
+        <option value = 'victoria'> Victoria BC, Canada </option>
+        <option value = 'tofino'> Tofino BC, Canada </option> 
+    </div>
+    !-->
 </body>
-
 </html>
