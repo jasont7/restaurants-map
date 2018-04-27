bd08f6d94b061e58a19ca8cc83719fb94b919ae6
Eric Hasegawa <erichasegawa1@gmail.com>
2018-04-27 11:08:55 -0700

    Update landingpage.php

--- Display/landingpage.php
+++ Display/landingpage.php
@@ -1,15 +1,33 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-    <title>FoodAdvisor</title>
-    <link rel="stylesheet" href="styles/landingpage.css" />
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet">
+<link rel = "stylesheet" href="styles/landing-style.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet">
+<title>Restaurant Map Landing</title>
 </head>
-<body>
-    <img id="logo" src="images/logo.png">
 
-    <div id="text">
-        <h1>I want... ________</h1> 
+<body>
+<div id = 'logodiv'>
+<img id = 'logo'  src = "images/custom_logo.png">
+</div> 
+<section class="wrapper">
+
+<h2 class="sentence">I want 
+    <div class="slidingVertical">
+      <span>a quick snack.</span>
+      <span>drinks with friends.</span>
+      <span>something new.</span>
+      <span>fast food.</span>
+      <span>something special.</span>
     </div>
+  </h2>
+  
+  </section>
+  <div id = "buttonDiv"> <a href="http://localhost/Display/">
+  <button class="button" style="vertical-align:middle"> <span> Enter the site </span> </button> 
+  </a>
+  </div>
+
 </body>
+
 </html>
