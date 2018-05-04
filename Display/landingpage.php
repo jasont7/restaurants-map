dd2c466fa553c04b7b9e5acd84c6201d5db0d3de
Eric Hasegawa <erichasegawa1@gmail.com>
2018-05-04 10:53:07 -0700

    Update landingpage.php

--- Display/landingpage.php
+++ Display/landingpage.php
@@ -1,25 +1,14 @@
 <!DOCTYPE html>
 
-<?PHP 
-
-require 'connect.php';
-
-// Select DB
-$db = mysqli_select_db($conn, "webapp");
-if (!$db) {
-    die ('Can\'t use db : ' . mysql_error());
-}
-
-?>
 <html>
 <head>
     <title>Food Advisor</title>
     <link rel="stylesheet" href="styles/landing-style.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+	<link rel="stylesheet" href="styles/autocomplete.css" />
 </head>
 <body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/awesomplete/1.1.2/awesomplete.js" async></script>
     <div id="logodiv">
         <img id="logo" src="images/custom_logo.png">
     </div>
@@ -39,7 +28,7 @@ if (!$db) {
         <input id="categoryInput" name="category" type="text" placeholder="Pizza, Burgers, Seafood..."/>
 
         <span class="icon"> <i class="fas fa-location-arrow"> </i> </span>
-        <input class = "awesomeplete"  id="cityInput" name="city" type="text" placeholder="City"/>
+        <input class = "awesomplete"  id="cityInput" name="city" type="text" placeholder="City"/>
 
         <input id="searchBtn" class="btnMain" value="Find" type="submit"/>
     </form>
@@ -48,18 +37,19 @@ var userCity = document.getElementById('cityInput');
 var userCat = document.getElementById('categoryInput');
 
 var autoCity = new Awesomplete(userCity, {
-        list: categories, //LOCATE LIST
+        list: ["Victoria", "Oak Bay"],
         filter: Awesomplete.FILTER_STARTSWITH,
         minChars: 1
     });
 
-var autoCat = new Awesomeplete(userCat, {
-		list: ["Victoria", "Oak Bay"],
+/*var autoCat = new Awesomplete(userCat, {
+		list: categories , //LOCATE
 		filter: Awesomplete.FILTER_STARTSWITH,
         minChars: 1
-}
+}); */
 
 </script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/awesomplete/1.1.2/awesomplete.js" async></script>
 
 </body>
 </html>
